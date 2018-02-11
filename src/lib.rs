9ad920d8f67f5bc9a6691ea3e78195762e18b2b8
Pedro Côrte-Real <pedro@pedrocr.net>
2018-02-11 14:35:26 +0000

    Move background work into multiple threads

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,41 @@ fn fix_lifetime<'a>(t: FS<'a>) -> FS<'static> {
   unsafe { mem::transmute(t) }
 }
 
+struct BackgroundThread {
+  handle: crossbeam::ScopedJoinHandle<()>,
+  tx: std::sync::mpsc::Sender<u8>,
+}
+
+impl BackgroundThread {
+  fn new<'a, F: 'a>(scope: &crossbeam::Scope<'a>, secs: u64, closure: F) -> Self
+  where F: Fn()+Send {
+    let (tx, rx) = mpsc::channel();
+
+    let handle = scope.spawn(move || {
+      // Sync the backing store to disk/remote every so often
+      let dur = time::Duration::from_secs(secs);
+      loop {
+        match rx.recv_timeout(dur) {
+          Err(mpsc::RecvTimeoutError::Timeout) => {
+            closure();
+          },
+          _ => break,
+        }
+      }
+    });
+
+    Self {
+      handle,
+      tx,
+    }
+  }
+
+  fn join(self) {
+    self.tx.send(0).unwrap(); // Signal the thread to die
+    self.handle.join();
+  }
+}
+
 pub fn run(source: &str, server: &str, mount: &str) -> Result<(), Error> {
   let bs = match BackingStore::new(source, server) {
     Ok(bs) => bs,
@@ -33,23 +68,11 @@ pub fn run(source: &str, server: &str, mount: &str) -> Result<(), Error> {
   };
   let fs = fix_lifetime(fs);
   let bsref = &bs;
-  let (tx, rx) = mpsc::channel();
 
   crossbeam::scope(|scope| {
-    let synchandle = scope.spawn(move || {
-      // Sync the backing store to disk/remote every so often
-      let dur = time::Duration::from_secs(10);
-      loop {
-        match rx.recv_timeout(dur) {
-          Err(mpsc::RecvTimeoutError::Timeout) => {
-            bsref.sync_all().unwrap();
-            bsref.do_uploads();
-            bsref.do_removals();
-          },
-          _ => break,
-        }
-      }
-    });
+    let sync   = BackgroundThread::new(&scope, 60, move || bsref.sync_all().unwrap());
+    let upload = BackgroundThread::new(&scope, 10, move || bsref.do_uploads());
+    let remove = BackgroundThread::new(&scope, 10, move || bsref.do_removals());
 
     let fshandle = scope.spawn(move || {
       let fs_mt = FuseMT::new(fs, 16);
@@ -58,8 +81,9 @@ pub fn run(source: &str, server: &str, mount: &str) -> Result<(), Error> {
     });
 
     let ret = fshandle.join();
-    tx.send(0).unwrap(); // Signal the fsync thread to die
-    synchandle.join();
+    sync.join();
+    upload.join();
+    remove.join();
     ret
   })
 }
