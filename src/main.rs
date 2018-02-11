82765bb2b4c34adb6e9826972a88d8fb72b8869b
Pedro Côrte-Real <pedro@pedrocr.net>
2018-02-11 20:05:08 +0000

    Pass config as arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 extern crate syncer;
+use std::env;
+use std::process;
+
+fn usage() {
+  eprintln!("USAGE: syncer <local source> <remote source> <mount point>");
+  process::exit(2);
+}
 
 fn main() {
-  let source = "data".to_string();
-  let path = "mnt".to_string();
-  let server = "localhost:~/blobs/".to_string();
+  let args: Vec<String> = env::args().collect();
+  if args.len() != 4 { usage() }
+
+  let source = &args[1];
+  let server = &args[2];
+  let path = &args[3];
   println!("Starting filesystem from {:?} and {:?} in {:?}", source, server, path);
   match syncer::run(&source, &server, &path) {
     Ok(_) => {},
